bb5e3d7a1bfd89a7508aff6a05b8752cf9e3c368
Kirill Verkhozin <kirill.verkhozin@example.com>
2026-04-21 20:19:38 +0800

    приложение по методичке обработка событий

---
+++ Event_processing/Objects/Marker.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Drawing.Drawing2D;
+
+namespace Event_processing.Objects
+{
+    class Marker : BaseObject
+    {
+        public Marker(float x, float y, float angle) : base(x, y, angle)
+        {
+        }
+
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Red), -3, -3, 6, 6);
+            g.DrawEllipse(new Pen(Color.Red, 2), -6, -6, 12, 12);
+            g.DrawEllipse(new Pen(Color.Red, 2), -10, -10, 20, 20);
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = base.GetGraphicsPath();
+            // буду использовать самый маленький круг в центре маркера
+            path.AddEllipse(-3, -3, 6, 6);
+            return path;
+        }
+    }
+}
